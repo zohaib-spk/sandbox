548d6e334136ebc5198439cef712cdd4e1449fe7
Zohaib_Spk <zohaib.akram3536@gmail.com>
2019-12-18 11:24:28 +0500

    print hello world

--- es6_learning/src/Person.js
+++ es6_learning/src/Person.js
@@ -1,5 +1,11 @@
 class Person{
     constructor(name){
-        this.name=namae;
+        this.name=name;
+    }
+
+    greet(){
+        return this.name + 'say hello!';
     }
 }
+
+console.log(new Person('Zohiab Akram').greet());
