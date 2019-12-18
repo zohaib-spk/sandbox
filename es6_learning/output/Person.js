6b8630ed59daa3e7e15d112abb9491af6a8134f2
Zohaib_Spk <zohaib.akram3536@gmail.com>
2019-12-18 12:27:04 +0500

    use extra paramter with spread operator

--- es6_learning/output/Person.js
+++ es6_learning/output/Person.js
@@ -1,14 +1,14 @@
 "use strict";
 
-function sumNumbers() {
-  for (var _len = arguments.length, numbers = new Array(_len), _key = 0; _key < _len; _key++) {
-    numbers[_key] = arguments[_key];
+function sumNumbers(value) {
+  for (var _len = arguments.length, numbers = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    numbers[_key - 1] = arguments[_key];
   }
 
   return numbers.reduce(function (prev, current) {
     return prev + current;
-  });
+  }) + value;
 }
 
-var numbers = [1, 2, 3, 4, 5, 6];
-console.log(sumNumbers.apply(void 0, numbers));
+var numbers = [1, 2, 3];
+console.log(sumNumbers.apply(void 0, [4].concat(numbers)));
--- es6_learning/src/Person.js
+++ es6_learning/src/Person.js
@@ -1,6 +1,6 @@
-function sumNumbers(...numbers) {
-    return numbers.reduce((prev, current) => prev + current);
+function sumNumbers(value,...numbers) {
+    return numbers.reduce((prev, current) => prev + current)+value;
 }
 
-let numbers = [1, 2, 3, 4, 5, 6];
-console.log(sumNumbers(...numbers));
+let numbers = [1, 2, 3];
+console.log(sumNumbers(4, ...numbers));
