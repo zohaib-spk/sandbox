6519df8c415551a3a9514687653c2c8f51521f55
Zohaib_Spk <zohaib.akram3536@gmail.com>
2019-12-18 17:32:22 +0500

    classes(super function)

--- es6_learning/app.js
+++ es6_learning/app.js
@@ -1,3 +1,37 @@
-let number = [1,2,3,4,5];
-number=number.map((number)=>number+'1');
-console.log(number);
+class Rectangle {
+    constructor(height, width) {
+        this.name = 'Rectangle';
+        this.height = height;
+        this.width = width;
+    }
+
+    sayName() {
+        console.log('Hi, I am a ', this.name + '.');
+    }
+
+    get area() {
+        return this.height * this.width;
+    }
+
+    set area(value) {
+        this._area = value;
+    }
+}
+
+class Square extends Rectangle {
+    constructor(length) {
+        super();
+        // Here, it calls the parent class's constructor with lengths
+        // provided for the Rectangle's width and height
+
+
+        // Note: In derived classes, super() must be called before you
+        // can use 'this'. Leaving this out will cause a reference error.
+        this.name = 'Square';
+        this.length=length;
+    }
+}
+
+var user = new Square(23);
+console.log(user.length);
+console.log(user.name);
