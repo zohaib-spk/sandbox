f16adb7d657eca9201dc980ad45892607b828ff2
Zohaib_Spk <zohaib.akram3536@gmail.com>
2019-12-18 12:11:03 +0500

    default parameters

--- es6_learning/output/Person.js
+++ es6_learning/output/Person.js
@@ -1,36 +1,12 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function defaultDiscount() {
+  return .10;
+}
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function applyDiscount(cost) {
+  var discount = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : defaultDiscount();
+  return cost - cost * discount;
+}
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var TaskCollection =
-/*#__PURE__*/
-function () {
-  function TaskCollection() {
-    var task = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
-
-    _classCallCheck(this, TaskCollection);
-
-    this.task = task;
-  }
-
-  _createClass(TaskCollection, [{
-    key: "log",
-    value: function log() {
-      this.task.forEach(function (task) {
-        return console.log(task);
-      });
-    }
-  }]);
-
-  return TaskCollection;
-}();
-
-var Task = function Task() {
-  _classCallCheck(this, Task);
-};
-
-new TaskCollection([new Task(), new Task(), new Task()]).log();
+alert(applyDiscount(100));
--- es6_learning/src/Person.js
+++ es6_learning/src/Person.js
@@ -1,39 +1,9 @@
-class TaskCollection {
-    constructor(task = []) {
-        this.task = task;
-    }
-
-    log() {
-        this.task.forEach(task => console.log(task))
-    }
-    //error:: this keyword is not access within the function
-    // prepare() {
-    //     this.task.forEach(function (task) {
-    //         console.log(this);
-    //     })
-    // }
-
-    prepare2() {
-        this.task.forEach(() => console.log(this)
-        )
-    }
-
-    // log2() {
-    //     this.task.forEach(task => {
-    //         console.log(task)
-    //     })
-    // }
-    //
-    // log3() {
-    //     this.task.forEach((task,index) => {
-    //         console.log(task,index)
-    //     })
-    // }
+function defaultDiscount() {
+    return .10;
 }
 
-class Task {
+function applyDiscount(cost, discount = defaultDiscount()) {
+    return cost - (cost * discount);
 }
 
-new TaskCollection(
-    [new Task, new Task, new Task]
-).log();
+alert(applyDiscount(100));
