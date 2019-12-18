073d7092c8870ab6c85bea02ddffb3995b635b97
Zohaib_Spk <zohaib.akram3536@gmail.com>
2019-12-18 12:05:54 +0500

    Arrow

--- es6_learning/output/Person.js
+++ es6_learning/output/Person.js
@@ -1,12 +1,36 @@
 "use strict";
 
-function fire(bool) {
-  if (bool) {
-    var foo = "bar";
-    console.log(foo);
-  } else {
-    console.log(foo);
-  }
-}
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-fire(false);
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var TaskCollection =
+/*#__PURE__*/
+function () {
+  function TaskCollection() {
+    var task = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
+
+    _classCallCheck(this, TaskCollection);
+
+    this.task = task;
+  }
+
+  _createClass(TaskCollection, [{
+    key: "log",
+    value: function log() {
+      this.task.forEach(function (task) {
+        return console.log(task);
+      });
+    }
+  }]);
+
+  return TaskCollection;
+}();
+
+var Task = function Task() {
+  _classCallCheck(this, Task);
+};
+
+new TaskCollection([new Task(), new Task(), new Task()]).log();
--- es6_learning/src/Person.js
+++ es6_learning/src/Person.js
@@ -1,11 +1,39 @@
-function fire(bool) {
-    if (bool) {
-        var foo = "bar";
-        // let foo = "bar"; block level
-        console.log(foo);
+class TaskCollection {
+    constructor(task = []) {
+        this.task = task;
     }
-    else {
-        console.log(foo);
+
+    log() {
+        this.task.forEach(task => console.log(task))
     }
+    //error:: this keyword is not access within the function
+    // prepare() {
+    //     this.task.forEach(function (task) {
+    //         console.log(this);
+    //     })
+    // }
+
+    prepare2() {
+        this.task.forEach(() => console.log(this)
+        )
+    }
+
+    // log2() {
+    //     this.task.forEach(task => {
+    //         console.log(task)
+    //     })
+    // }
+    //
+    // log3() {
+    //     this.task.forEach((task,index) => {
+    //         console.log(task,index)
+    //     })
+    // }
 }
-fire(false);
+
+class Task {
+}
+
+new TaskCollection(
+    [new Task, new Task, new Task]
+).log();
