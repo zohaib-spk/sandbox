2574a01919c657e8f93128ff9f9cc36966554652
Zohaib_Spk <zohaib.akram3536@gmail.com>
2019-12-18 17:14:10 +0500

    map in javascript

--- es6_learning/app.js
+++ es6_learning/app.js
@@ -1,24 +1,3 @@
-let items = new Set(['one', 'two', 'three']);
-console.log(items);  // [one,two,three]
-
-
-items.add('three');
-console.log(items);  // [one,two,three]
-
-
-items.add('four');
-console.log(items);  // [one,two,three,four]
-
-items.delete('four');
-console.log(items);  // [one,two,three]
-
-console.log(items.size);//3
-
-
-console.log('items has 4?',items.has('four'));
-
-items.forEach((item) => console.log(item));
-
-items.clear();
-console.log(items);
-
+let number = [1,2,3,4,5];
+number=number.map((number)=>number+'1');
+console.log(number);
