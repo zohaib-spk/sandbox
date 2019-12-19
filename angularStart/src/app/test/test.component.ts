92f96950ed9a3b6ae03e691b0ae4ed3ed9817d41
Zohaib_Spk <zohaib.akram3536@gmail.com>
2019-12-19 15:35:54 +0500

    interpolation

--- angularStart/src/app/test/test.component.ts
+++ angularStart/src/app/test/test.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+    selector: 'app-test',
+    template: `
+        <h3>My name is {{name}}</h3>
+        <h3>{{getWelcomeMessage()}}</h3>
+        <h4>Site Url : {{getSiteUrl()}}</h4>
+    `,
+    styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+    public name = 'Zohaib Akram';
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    getWelcomeMessage() {
+        return 'Welcome ' + this.name;
+    }
+
+    getSiteUrl() {
+        return window.location.href;
+    }
 
 }
